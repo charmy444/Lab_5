0d7318bf99dc57bbdda2d582db87dc97a28533a5
Baryshnikov Egor Alexandrovich <eabaryshnikov@mai.education>
2025-12-24 22:03:10 +0300

    2 ошибка

--- project/src/main.py
+++ project/src/main.py
@@ -3,7 +3,6 @@ try:
 except ImportError:
     from simulation import run_simulation
 
-
 def main() -> None:
     try:
         steps_input = input("Введите количество шагов симуляции (по умолчанию 20): ").strip()
@@ -14,13 +13,13 @@ def main() -> None:
                 return
         else:
             steps = 20
-        
+
         seed_input = input("Введите seed для генератора случайных чисел (опционально, Enter для пропуска): ").strip()
         seed = int(seed_input) if seed_input else None
     except ValueError:
         print("Ошибка: введите целое число")
         return
-    
+
     run_simulation(steps=steps, seed=seed)
 
 
